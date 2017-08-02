a810a0b1f0520a79bf54d182ef5604c688b8ce77
ojha-url <ojha@mit.edu>
2017-08-02 18:50:09 -0400

    Added pre render pipeline step

--- src/app.js
+++ src/app.js
@@ -4,11 +4,23 @@ export class App {
     config.title = 'PetaBencana.id';
     config.options.pushState = true;
     config.options.root = '/';
+    config.addPreRenderStep(PreRenderStep);
+
     config.map([
-      {route: ['', 'map', 'map/:city', 'map/:city/:report'],   name: 'map',  moduleId: 'routes/landing/landing'},
       {route: 'cards/:id',                                                   moduleId: 'routes/cards/cards'}
     ]);
     config.mapUnknownRoutes({moduleId: 'routes/landing/landing'});
     this.router = router;
+  }
+}
+
+export class PreRenderStep {
+  run(navigationInstruction, Next) {
+    this.next = Next;
+    console.log('Inside pre render');
+
+    return new Promise((resolve, reject) => {
+      resolve(this.next());
+    });
   }
 }
