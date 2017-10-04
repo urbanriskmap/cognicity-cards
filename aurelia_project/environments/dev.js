0f396cde4d95230c8f01eb78e8cc5371c149c167
Matthew Berryman <matthew@acrossthecloud.net>
2017-10-04 17:47:21 +0800

    dev hostname update for (cards-dev.)riskmap.us

--- aurelia_project/environments/dev.js
+++ aurelia_project/environments/dev.js
@@ -10,7 +10,7 @@ export default {
     default_language: 'en',
     tile_layer: 'https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidXJiYW5yaXNrbWFwIiwiYSI6ImNpdmVhbTFraDAwNHIyeWw1ZDB6Y2hhbTYifQ.tpgt1PB5lkJ-wITS02c96Q',
     data_server: 'https://data-dev.riskmap.us/',
-    app: 'https://broward-dev.riskmap.us/'
+    app: 'https://dev.riskmap.us/'
   },
   petabencana: {
     title: 'PetaBencana.id',
