5ddb7024360adfb7f8f8660ea777751715a1de9f
ojha-url <mayank.ojha@gmail.com>
2018-07-26 14:24:19 -0400

    Added hindi locales for Madhubani

--- aurelia_project/environments/dev.js
+++ aurelia_project/environments/dev.js
@@ -22,8 +22,8 @@ export default {
   },
   riskmap_in: {
     title: 'RiskMap.in',
-    supported_languages: ['en', 'tm'],
-    default_language: 'en',
+    supported_languages: ['en', 'hi'],
+    default_language: 'hi',
     tile_layer: 'https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidXJiYW5yaXNrbWFwIiwiYSI6ImNqMnFraWVzYzAyd24ycXRqMmpvbmhyZ2QifQ.xc_v7umok760t2q6NZK1RA',
     data_server: 'https://data-dev.riskmap.in/',
     app: 'https://dev.riskmap.in/'
