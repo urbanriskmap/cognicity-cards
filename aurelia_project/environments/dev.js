f841f1f13d717c7c5ff8ed280d6baf99d46d2523
asbarve <adityasbarve@gmail.com>
2018-07-27 13:21:56 -0400

    changed mapstyle to show madhubani

--- aurelia_project/environments/dev.js
+++ aurelia_project/environments/dev.js
@@ -24,7 +24,7 @@ export default {
     title: 'RiskMap.in',
     supported_languages: ['en', 'hi'],
     default_language: 'en',
-    tile_layer: 'https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidXJiYW5yaXNrbWFwIiwiYSI6ImNqMnFraWVzYzAyd24ycXRqMmpvbmhyZ2QifQ.xc_v7umok760t2q6NZK1RA',
+    tile_layer: 'https://api.mapbox.com/styles/v1/urbanriskmap/cjfvacwic1cfc2smiwbyfwcs4/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidXJiYW5yaXNrbWFwIiwiYSI6ImNqMnFraWVzYzAyd24ycXRqMmpvbmhyZ2QifQ.xc_v7umok760t2q6NZK1RA',
     data_server: 'https://data-dev.riskmap.in/',
     app: 'https://dev.riskmap.in/'
   }
