6da85e9583774397323e6650cdab54dad3516975
asbarve <adityasbarve@gmail.com>
2018-08-22 10:59:35 -0400

    added assessment-slider

--- src/components/assessment-slider/assessment-slider.js
+++ src/components/assessment-slider/assessment-slider.js
@@ -1,9 +1,9 @@
 import {bindable, customElement} from 'aurelia-framework';
 
 //start-aurelia-decorators
-@customElement('assessment-content')
+@customElement('assessment-slider')
 //end-aurelia-decorators
-export class AssessmentContent {
+export class AssessmentSlider {
   //@bindable attributes should have no case, eg. reportcard
   //@bindable functions should be in camelCase, then in html template usage, use camel-case.call
   //start-aurelia-decorators
