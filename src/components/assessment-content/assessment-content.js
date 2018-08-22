6b8b6d68ddf49216249f90b9fa77b042d5d3f0e2
asbarve <adityasbarve@gmail.com>
2018-08-22 13:12:59 -0400

    damage selection buttons needs review

--- src/components/assessment-content/assessment-content.js
+++ src/components/assessment-content/assessment-content.js
@@ -1,4 +1,5 @@
-import {bindable, customElement} from 'aurelia-framework';
+// import {bindable, customElement} from 'aurelia-framework';
+import {customElement} from 'aurelia-framework';
 
 //start-aurelia-decorators
 @customElement('assessment-content')
@@ -7,15 +8,17 @@ export class AssessmentContent {
   //@bindable attributes should have no case, eg. reportcard
   //@bindable functions should be in camelCase, then in html template usage, use camel-case.call
   //start-aurelia-decorators
-  @bindable locale;
+  // @bindable locale;
   //end-aurelia-decorators
-
-  get x() {
-    return this.y;
-  }
+  damageSelectors = ['No', 'Yes'];
+  houseDamaged = null;
 
   attached() {
-  //
+    //
+  }
 
+  selectDamage(damaged) {
+    this.houseDamaged = damaged;
+    console.log(this.houseDamaged);
   }
 }
