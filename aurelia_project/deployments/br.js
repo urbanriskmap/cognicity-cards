86d704da834ea162abbe63daa34aef78a17d4c65
asbarve <adityasbarve@gmail.com>
2018-10-10 14:08:17 -0400

    added florida panhandle

--- aurelia_project/deployments/br.js
+++ aurelia_project/deployments/br.js
@@ -12,13 +12,21 @@ export default {
   height_units: 'in',
   map: {
     'instance_regions': {
-      'broward': {
+      'Broward': {
         'region': 'brw',
         'bounds': {
           'sw': [ 25.948143, -80.477974],
           'ne': [ 26.372556, -80.056669]
         },
         'center': [26.138301, -80.199261]
+      },
+      'Florida Panhandle': {
+        'region': 'fph',
+        'bounds': {
+          'sw': [ 27.2007, -87.7825],
+          'ne': [ 31.3291, -81.0455]
+        },
+        'center': [30.1588, -85.6602]
       }
     },
     'default_region': {
